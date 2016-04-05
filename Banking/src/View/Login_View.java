69e68f94c025284f10b9011a4002e252aab3fa55
Thanyalux <Kango_000@10.17.250.134>
2016-04-05 22:53:49 +0700

    Create View Package and Add Login_View class

--- Banking/src/View/Login_View.java
+++ Banking/src/View/Login_View.java
@@ -5,10 +5,42 @@
  */
 package View;
 
+import java.awt.*;
+import javax.swing.*;
+
 /**
  *
  * @author Kango_000
  */
-public class Login_View {
+
+public class Login_View 
+{
+    private JFrame frame;
+   // private JLabel usernameLabel;
+   // private JTextField usernameTextField;
+   // private JLabel passwordLabel;
+   // private JPasswordField passwordTextField;
+   // private JButton loginButton;
+    
+    public static void main (String [] args){
+       Login_View frame = new Login_View();
+       frame.setVisibel(true);
+    } 
+    
+    public Login_View(){
+        makeFrame();
+    }
+    
+    private void makeFrame(){
+        frame = new JFrame("Log in");
+        Container contentPane = frame.getContentPane();
+        frame.setVisible(true);
+    }
+
+    private void setVisibel(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+                  
     
 }
+
