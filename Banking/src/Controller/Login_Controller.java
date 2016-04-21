1294124fe8919477ebf3869fa5cf7c995141d512
Thanyalux <Kangonta_t@hotmail.com>
2016-04-21 18:20:02 +0700

    Create Login Controller class

---
+++ Banking/src/Controller/Login_Controller.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+import java.awt.event.ActionListener;
+
+import View.Login_View;
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ * @author Kango_000
+ */
+public class Login_Controller {
+    Login_View loginView;
+
+    public Login_Controller() {
+        loginView = new Login_View();
+        loginView.setVisible(true);
+        loginView.setActionLoginButton(new LoginAction());
+    }
+    
+    private class LoginAction implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            String username = loginView.getUername();
+            String password = loginView.getPassword();
+        }
+        
+    }
+    
+    public static void main(String[] args){
+        new Login_Controller();
+    }
+    
+    
+    
+}
