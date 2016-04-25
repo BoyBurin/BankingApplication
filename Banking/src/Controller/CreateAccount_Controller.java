1b3039d79f294e8ca2cee51e1df246542c30bc3c
Thanyalux <Kangonta_t@hotmail.com>
2016-04-25 14:59:45 +0700

    create CreateEmployeeAccount_Controller

---
+++ Banking/src/Controller/CreateAccount_Controller.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+import View.CreateAccount_View;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+/**
+ *
+ * @author Kango_000
+ */
+public class CreateAccount_Controller {
+    CreateAccount_View createAccView;
+    
+    public static void main (String[]ags){
+        
+    }
+    
+    public CreateAccount_Controller(){
+        createAccView = new CreateAccount_View();
+        createAccView.setVisible(true);
+        //cteateAccView.setActionLoginButton(new CreateAccount_Controller.createAccAction());
+    }
+    
+    private class createAccAction implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            String name = createAccView.getMiddleName();
+            String middleName = createAccView.getMiddleName();
+            String surname = createAccView.getSurname();
+            String username = createAccView.getUername();
+            String password = createAccView.getPassword();
+            String date = createAccView.getDate();
+            String email = createAccView.getEmail();
+            String phone = createAccView.getPhone();
+            String houseNo = createAccView.getHouseNo();
+            String soi = createAccView.getSoi();
+            String road = createAccView.getRoad();
+            String subDistrict = createAccView.getSubDistrict();
+            String district = createAccView.getDistrict();
+            String province = createAccView.getProvince();
+        }
+    }
+    
+}
