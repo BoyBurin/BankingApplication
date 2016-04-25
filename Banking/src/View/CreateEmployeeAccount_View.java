c6a2a6f583282ff551fc075d4ccdb70d7122f8f2
Thanyalux <Kangonta_t@hotmail.com>
2016-04-25 15:08:35 +0700

    Update UI

--- Banking/src/View/CreateEmployeeAccount_View.java
+++ Banking/src/View/CreateEmployeeAccount_View.java
@@ -18,7 +18,7 @@ public class CreateEmployeeAccount_View extends JFrame {
     
     private JTextField employeeNoField;
     private JTextField employeeNameField;
-    private JButton createButton;
+    private JButton backButton;
     
     public static void main(String[]args){
         new CreateEmployeeAccount_View().setVisible(true);
@@ -27,13 +27,14 @@ public class CreateEmployeeAccount_View extends JFrame {
     public CreateEmployeeAccount_View(){
         makeFrame();
         inComponent();
+        backButton();
     }
     
     public void makeFrame(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	setBounds(100, 100, 454, 343);
-        setSize(new java.awt.Dimension(700, 700));
-	setTitle("Create Employee Account");
+        setSize(new java.awt.Dimension(700, 500));
+	setTitle("Employee Account");
 	getContentPane().setLayout(null);
     }
     
@@ -41,33 +42,32 @@ public class CreateEmployeeAccount_View extends JFrame {
         JLabel employeeNoLable = new JLabel("Employee No. :");
 	employeeNoLable.setBounds(40, 40, 100, 30);
 	getContentPane().add(employeeNoLable);
-        
-        employeeNoField = new JTextField("");
-	employeeNoField.setBounds(150, 40, 160, 30);
-	getContentPane().add(employeeNoField);
-        
+                      
         JLabel nameLable = new JLabel("Name :");
 	nameLable.setBounds(40, 80, 80, 30);
 	getContentPane().add(nameLable);
         
-        employeeNameField = new JTextField("");
-	employeeNameField.setBounds(150, 80, 160, 30);
-	getContentPane().add(employeeNameField);
+        JLabel middleNameLable = new JLabel("Middle Name :");
+	middleNameLable.setBounds(40, 120, 80, 30);
+	getContentPane().add(middleNameLable);
+        
+        JLabel surnameLable = new JLabel("Surname :");
+	surnameLable.setBounds(40, 160, 80, 30);
+	getContentPane().add(surnameLable);
+        
+        JLabel usernameLable = new JLabel("Username :");
+	usernameLable.setBounds(40, 200, 80, 30);
+	getContentPane().add(usernameLable);
+        
+        JLabel passwordLable = new JLabel("Pasword :");
+	passwordLable.setBounds(40, 240, 80, 30);
+	getContentPane().add(passwordLable);
+          
     }
     
-    public void createButton(){
-        createButton = new JButton("Create");
-        createButton.setBounds(350, 520, 100, 30);
-	getContentPane().add(createButton);
-    }
-    
-    public String getEmployeeNo(){
-        String employeeNo = employeeNoField.getText();
-        return employeeNo;
-    }
-    
-    public String getEmployeeName(){
-        String employeeName = employeeNameField.getText();
-        return employeeName;
+    public void backButton(){
+        backButton = new JButton("Back");
+        backButton.setBounds(300, 350, 100, 30);
+	getContentPane().add(backButton);
     }
 }
